5680a6d480f7e6a8497c17ba6b0bd352829c9654
Super User <101892232+i-douk@users.noreply.github.com>
2024-09-13 01:39:37 +0100

    ex3

--- todo_app/app.js
+++ todo_app/app.js
@@ -1,9 +1,10 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const dotenv = require('dotenv');
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('DevOps with Kubernetes!')
 })
 
