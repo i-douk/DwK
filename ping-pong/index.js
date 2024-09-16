90d594028ab4ce79f1feea11afef328bf576f26d
i-douk <101892232+i-douk@users.noreply.github.com>
2024-09-16 22:25:37 +0200

    ping

---
+++ ping-pong/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+let count = 0;
+const counter = () => {
+    return ++count;
+}
+
+app.get('/pingpong', (_req, res) => {
+    res.status(200).send(`pong ${counter()}`);
+});
+  
+  app.listen(port, () => {
+      console.log(`Server started in port  ${port}`)
+  })
