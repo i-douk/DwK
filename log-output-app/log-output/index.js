402eaba430261da3f21ef47a5ea447c04953436a
i-douk <101892232+i-douk@users.noreply.github.com>
2024-10-20 23:21:30 +0100

    2.06:incomplete

--- log-output-app/log-output/index.js
+++ log-output-app/log-output/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '/usr/src/app/manifests/information.env' });
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
