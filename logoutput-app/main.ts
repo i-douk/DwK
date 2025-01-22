0de69ffb07b33f27251bc365bda1cef07984d864
i-douk <101892232+i-douk@users.noreply.github.com>
2025-01-22 23:13:46 +0100

    1.01

---
+++ logoutput-app/main.ts
@@ -0,0 +1,24 @@
+import * as uuid from "jsr:@std/uuid";
+
+const handler = (_req: Request): Response => {
+  let timer: number;
+  const randomString = uuid.v1.generate() 
+  const body = new ReadableStream({
+    async start(controller) {
+      timer = setInterval(() => {
+        controller.enqueue(new Date().toISOString() + ": " + randomString + '\n')
+        console.log(new Date().toISOString() + ": " + randomString)
+      }, 4000);
+    },
+    cancel() {
+      clearInterval(timer);
+    },
+  });
+  return new Response(body.pipeThrough(new TextEncoderStream() ), {
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+    },
+  });
+};
+
+Deno.serve({ port: 4242, hostname: "0.0.0.0" }, handler);
