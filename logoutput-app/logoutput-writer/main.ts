dda1e62dd1d73dc0b2840fed491e7c4ead7a8042
i-douk <101892232+i-douk@users.noreply.github.com>
2025-03-13 17:35:21 +0000

    fix writer

--- logoutput-app/logoutput-writer/main.ts
+++ logoutput-app/logoutput-writer/main.ts
@@ -7,18 +7,20 @@ const randomString = uuid.v1.generate();
 const encoder = new TextEncoder();
 
 let timer: number;
+const writer = openFile.writable.getWriter();
 
 const generateLogs = () => {
   timer = setInterval(async () => {
     const logEntry = new Date().toISOString() + ": " + randomString + "\n";
-    const writer = openFile.writable.getWriter();
     try {
       await writer.write(encoder.encode(logEntry));
-      await writer.close();
       openFile.close();
     } catch (error) {
       console.error("Failed to write log entry:", error);
+    } finally {
+      await writer.close();
     }
   }, 5000);
 };
+
 generateLogs();
