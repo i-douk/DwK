42171679655b002529e8793b9b572a032b80f7c8
i-douk <101892232+i-douk@users.noreply.github.com>
2025-03-14 23:45:15 +0000

    1.12

---
+++ todo-app/deps.ts
@@ -0,0 +1,2 @@
+export { readerFromStreamReader } from "https://deno.land/std@0.221.0/io/reader_from_stream_reader.ts";
+export { copy } from "https://deno.land/std@0.221.0/io/copy.ts";
--- todo-app/main.ts
+++ todo-app/main.ts
@@ -1,5 +1,35 @@
-const handler = async (_req : Request):Promise<Response> => {
-  return new Response("Server running on port 4243");
-}
- 
-Deno.serve({port: 4243},  handler);
+import { fetchAndSaveImage } from "./utils.ts";
+
+// Fetch and save image
+let imagePath = await fetchAndSaveImage();
+setInterval(async () => {
+  imagePath = await fetchAndSaveImage();
+}, 3600);
+
+const page = `
+
+  <div class="container center">
+    <img src="/image.png" alt="image" />
+  </div>
+`;
+
+
+// Handler for serving the image with caching
+const handler = async (_req: Request) => {
+  try {
+    const image = await Deno.readFile(imagePath);
+
+    return new Response(image, { 
+      headers: {
+        "Content-Type": "image/png",
+        "Cache-Control": "public, max-age=3600, immutable", 
+        "ETag": `"${imagePath}"`,
+      },
+    });
+  } catch (error) {
+    return new Response("Image not found", { status: 404 });
+  }
+};
+
+// Start the server
+Deno.serve({ port: 4243 }, handler);
