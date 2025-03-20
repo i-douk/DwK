7464fcaa2a41ce62b6eb668d297d5169b8a89a1d
i-douk <101892232+i-douk@users.noreply.github.com>
2025-03-20 03:32:22 +0000

    2.07 completed

--- ping-pong/libs.ts
+++ ping-pong/libs.ts
@@ -3,8 +3,8 @@ import { Client } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 export const sql = new Client({
     user: "postgres",
     database: "postgres",
-    hostname: "postgres-svc.log-pingpong-ns.svc.cluster.local:5432",
+    hostname: "postgres-svc.log-pingpong-ns.svc.cluster.local",
     port: 5432,
+    password: Deno.env.get('POSTGRES_PASSWORD'),
   });
 
-await sql.connect();
--- ping-pong/main.ts
+++ ping-pong/main.ts
@@ -1,24 +1,44 @@
 import { sql } from "./libs.ts";
-console.log(sql)
-console.log('connection established')
-let count = 0;
+
+//fetch log to save and print
 let log: ArrayBufferLike;
 try {
    log = await fetch(`http://logoutput-svc.log-pingpong-ns.svc.cluster.local:2346/logs`).then(res => res.arrayBuffer());
+   sql.connect
 } catch (error) {
   console.error("Failed to fetch logs:", error);
   log = new TextEncoder().encode("Logs unavailable").buffer;
-
 }
+
+//connect to db and save
+async function saveLog(level: string) {
+  await sql.connect()
+  try {
+    await sql.queryObject`
+      INSERT INTO logs (level)
+      VALUES (${level})
+    `;
+    console.log("Log saved successfully");
+  } catch (error) {
+    console.error("Failed to save log:", error);
+  } finally {
+    await sql.end();
+  }
+}
+
+
+let count = 0;
 const handler =  async (_req: Request): Promise<Response> => {
   const decoder = new TextDecoder();
+  saveLog(decoder.decode(new Uint8Array(await log)))
 
   try {
     return new Response(`
       file content : ${Deno.env.get('FILE_CONTENT')} \n
       env variable : ${Deno.env.get('MESSAGE')} \n
       ${decoder.decode(new Uint8Array(await log))} \n
-      Ping/Pongs : ${count++}`, {
+      Ping/Pongs : ${count++} \n
+      `, {
       headers: { "Content-Type": "text/plain" },
     });
   } catch (error) {
