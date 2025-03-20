217eb1ca340b869d6cbc87cf705c6fe9b98adab9
i-douk <101892232+i-douk@users.noreply.github.com>
2025-03-20 22:49:08 +0000

    2.08 corrected

--- todo-app/main.ts
+++ todo-app/main.ts
@@ -10,7 +10,7 @@ import { Client } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 const sql = new Client({
     user: "postgres",
     database: "postgres",
-    hostname: "postgres-svc.log-pingpong-ns.svc.cluster.local",
+    hostname: "postgres-svc.todo-ns.svc.cluster.local",
     port: 5432,
     password: Deno.env.get('POSTGRES_PASSWORD'),
 });
